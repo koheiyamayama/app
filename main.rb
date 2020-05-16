838bfdfd900fc162ad872427fed820f6a9af1227
koheiyamaguchi0203 <kohei.yamaguchi846@litalico.co.jp>
2020-05-16 15:50:11 +0900

    DBとの接続を行った

--- main.rb
+++ main.rb
@@ -6,6 +6,8 @@ Bundler.require
 require 'sinatra'
 require 'mysql2'
 
+DB = Mysql2::Client.new(:host => "192.168.11.4", :username => "kohei", password: "h19970203")
+p DB
 get '/' do
   erb :index
 end
