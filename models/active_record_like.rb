1590239d50b5d56955fb7007104363c4f7c94d3a
koheiyamaguchi0203 <kohei.yamaguchi846@litalico.co.jp>
2020-05-23 03:15:03 +0900

    setterメソッドを定義した

--- models/active_record_like.rb
+++ models/active_record_like.rb
@@ -10,12 +10,24 @@ class ActiveRecordLike
     Mysql2::Client.new(host: '192.168.11.4', username: 'kohei', password: 'h19970203', database: 'app')
   end
 
-  def initialize(record)
+  def initialize(record: {})
+    record = if record&.empty?
+      hash = Hash.new
+      DB.query("show columns from todos;").each { |record| hash[record["Field"]] = "" }  
+      hash
+    else 
+      record
+    end
+
     record.each do |key, value|
+      # この辺無駄にイテレートしている気がするので、リファクタする
       instance_variable_set("@#{key}", value)
       instance_variables.each do |instance_variable|
         define_singleton_method(instance_variable.to_s.gsub('@', '')) do
           instance_variable_get(instance_variable)
+        end
+        define_singleton_method(instance_variable.to_s.gsub('@', '') + '=') do |arg|
+          instance_variable_set("@#{key}", arg)
         end
       end
     end
@@ -25,30 +37,28 @@ class ActiveRecordLike
     def all
       statement = DB.prepare("select * from #{to_table_name};")
       statement.execute.map do |record|
-        new(record)
+        new(record: record)
       end
     end
 
-    def insert(collection)
-      raise ArgumentError, 'Hashを渡してください' unless collection.instance_of?(Hash)
-      columns = collection.keys.join(',')
-      values = collection.values.map { |value| "'#{value}'"}.join(',')
+    def insert(hash)
+      raise ArgumentError, 'Hashを渡してください' unless hash.instance_of?(Hash)
+      columns = hash.keys.join(',')
+      values = hash.values.map { |value| "'#{value}'"}.join(',')
       statement = DB.prepare("insert into #{to_table_name} (#{columns}) values (#{values});")
       unless statement.execute
-        new(collection)
+        new(record: hash)
       end
     end
 
     def find(id)
       statement = DB.prepare("select * from #{to_table_name} where id = ?;")
       result = statement.execute(id).map do |record|
-        new(record)
+        new(record: record)
       end[0]
       raise StandardError, 'NotFound' unless result
       result
     end
-
-    private
 
     def to_table_name
       str = to_s
@@ -65,6 +75,9 @@ class ActiveRecordLike
   def destroy
   end
 
-  def update
+  def update(id)
+  end
+
+  def assign(hash)
   end
 end
